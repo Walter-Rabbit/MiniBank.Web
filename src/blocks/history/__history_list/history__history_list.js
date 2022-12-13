7244fa72a75ce98fc9aa4fe21f2748444cf83ebc
Valery Shevchenko <ar4er.valera@gmail.com>
2022-12-13 19:05:44 +0300

    feat: lab-6

--- src/blocks/history/__history_list/history__history_list.js
+++ src/blocks/history/__history_list/history__history_list.js
@@ -1,17 +1,48 @@
-window.addEventListener('load', function history__history_list() {
+window.addEventListener('load', async function history__history_list() {
   let ul = document.getElementById('main_page_history');
 
   if (ul === null) {
     return;
   }
 
-  let transactions = window.localStorage.getItem('history');
   let section = document.getElementById('main_page_section_history')
+  let history_list = document.getElementById('main_page_history')
+
+  let transactions = await fetch('https://639897dc044fa481d6a38d71.mockapi.io/Transaction', {
+    method: 'GET'
+  })
+    .then(response => response.text())
+    .then(text => JSON.parse(text));
+
+  for (let i = 0; i < 10; i++) {
+    let li = document.createElement('li');
+    let transaction = document.createElement('div');
+    transaction.className = 'transaction';
+
+    let p_amount = document.createElement('p');
+    let amount = transactions[i]['amount'];
+    if (amount >= 0) {
+      p_amount.className = 'transaction__positive_balance';
+    }
+    else {
+      p_amount.className = 'transaction__negative_balance';
+    }
+    p_amount.textContent = amount + 'руб.';
+    transaction.append(p_amount);
+
+    let p_date = document.createElement('p');
+    p_date.className = 'transaction__date';
+    let date = new Date(transactions[i]['date']);
+    p_date.textContent = `${date.getDate() + 1}.${date.getMonth() + 1}.${date.getFullYear()}`
+    transaction.append(p_date);
+
+    li.append(transaction);
+    history_list.append(li);
+  }
 
   if (section !== null) {
-    if (transactions !== null) {
+    if (history_list !== null) {
       section.style.visibility = 'visible';
-      ul.innerHTML = transactions;
     } else {
       section.style.visibility = 'hidden';
     }
--- src/imports/common_imports.js
+++ src/imports/common_imports.js
@@ -1,37 +1,45 @@
-window.addEventListener('load', function catalog__item_list__transactions() {
+window.addEventListener('load', async function catalog__item_list__transactions() {
   let ul = document.getElementById('transaction_page_history');
 
   if (ul === null) {
     return;
   }
 
-  let savedLis = window.localStorage.getItem('history');
+  let transactions = await fetch('https://639897dc044fa481d6a38d71.mockapi.io/Transaction', {
+    method: 'GET'
+  })
+    .then(response => response.text())
+    .then(text => JSON.parse(text));
 
-  let tempUl = document.createElement('ul');
-  tempUl.innerHTML = savedLis;
-
-  for (let elm of tempUl.children) {
+  for (let tr of transactions) {
     let li = document.createElement('li');
     let div = document.createElement('div');
     div.className = 'item'
+    let transaction = document.createElement('div');
+    transaction.className = 'item';
 
-    let p1 = document.createElement('p');
-    p1.className = 'item__title';
-    p1.textContent = elm.children[0].children[0].textContent;
+    let p_amount = document.createElement('p');
+    let amount = tr['amount'];
+    if (amount >= 0) {
+      p_amount.className = 'transaction__positive_balance';
+    } else {
+      p_amount.className = 'transaction__negative_balance';
+    }
+    p_amount.textContent = amount + 'руб.';
+    transaction.append(p_amount);
 
-    let p2 = document.createElement('p');
-    p2.className = 'item__description';
-    p2.textContent = 'Какое-то умное описание.';
+    let p_description = document.createElement('p');
+    p_description.className = 'item__description';
+    p_description.textContent = tr['target'];
+    transaction.append(p_description);
 
-    let p3 = document.createElement('p');
-    p3.className = 'item__date'
-    p3.textContent = elm.children[0].children[1].textContent;
+    let p_date = document.createElement('p');
+    p_date.className = 'transaction__date';
+    let date = new Date(tr['date']);
+    p_date.textContent = `${date.getDate() + 1}.${date.getMonth() + 1}.${date.getFullYear()}`
+    transaction.append(p_date);
 
-    div.append(p1);
-    div.append(p2);
-    div.append(p3);
-
-    li.append(div);
+    li.append(transaction);
     ul.append(li);
   }
 });
@@ -130,20 +138,51 @@ function function_list__button__make_transaction() {
   section[0].style.visibility = 'visible';
 }
 
-window.addEventListener('load', function history__history_list() {
+window.addEventListener('load', async function history__history_list() {
   let ul = document.getElementById('main_page_history');
 
   if (ul === null) {
     return;
   }
 
-  let transactions = window.localStorage.getItem('history');
   let section = document.getElementById('main_page_section_history')
+  let history_list = document.getElementById('main_page_history')
+
+  let transactions = await fetch('https://639897dc044fa481d6a38d71.mockapi.io/Transaction', {
+    method: 'GET'
+  })
+    .then(response => response.text())
+    .then(text => JSON.parse(text));
+
+  for (let i = 0; i < 10; i++) {
+    let li = document.createElement('li');
+    let transaction = document.createElement('div');
+    transaction.className = 'transaction';
+
+    let p_amount = document.createElement('p');
+    let amount = transactions[i]['amount'];
+    if (amount >= 0) {
+      p_amount.className = 'transaction__positive_balance';
+    }
+    else {
+      p_amount.className = 'transaction__negative_balance';
+    }
+    p_amount.textContent = amount + 'руб.';
+    transaction.append(p_amount);
+
+    let p_date = document.createElement('p');
+    p_date.className = 'transaction__date';
+    let date = new Date(transactions[i]['date']);
+    p_date.textContent = `${date.getDate() + 1}.${date.getMonth() + 1}.${date.getFullYear()}`
+    transaction.append(p_date);
+
+    li.append(transaction);
+    history_list.append(li);
+  }
 
   if (section !== null) {
-    if (transactions !== null) {
+    if (history_list !== null) {
       section.style.visibility = 'visible';
-      ul.innerHTML = transactions;
     } else {
       section.style.visibility = 'hidden';
     }
